eabaae5abcd0085447d3803b65f20dab7b4df172
IgorPugachev <igor0514@gmail.com>
2021-05-01 23:45:44 +1000

    Tasks 1-1 and 1-3 are done.

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,20 @@
 function ucFirst(str) {
-  // ваш код...
+  // ваш код...В RunJS код работает корректно для всех трех заданных значений.
+  if (str === '') {
+    str = Number(str);
+    if (str === 0) {
+      str = '';
+      return str;
+      // console.log(str);
+      // console.log(typeof str);
+    }
+
+  } else {
+    let firstLetter = str[0].toUpperCase();
+    let otherLetters = str.substring(1);
+    str = String(`${firstLetter}${otherLetters}`);
+    return str;
+    // console.log(str);
+    // console.log(typeof str);
+  }
 }
